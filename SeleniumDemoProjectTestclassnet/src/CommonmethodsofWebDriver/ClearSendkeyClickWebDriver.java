5f01273f3305b37eb0f54cbe88a373b5da0d0dd8
Kadeer Ainiwaer <49220464+anwarkadir76@users.noreply.github.com>
2019-09-22 15:13:50 -0400

    Update ClearSendkeyClickWebDriver.java

--- SeleniumDemoProjectTestclassnet/src/CommonmethodsofWebDriver/ClearSendkeyClickWebDriver.java
+++ SeleniumDemoProjectTestclassnet/src/CommonmethodsofWebDriver/ClearSendkeyClickWebDriver.java
@@ -8,31 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class ClearSendkeyClickWebDriver {
 
 	 public static void main(String[] args) {
-package CommonmethodsofWebDriver;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-public class ClearSendkeyClickWebDriver {
-
-	 public static void main(String[] args) {
 		 System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
-		    WebDriver driver = new ChromeDriver();
-		    driver.get("https://www.baidu.com/");
-
-		    WebElement search_text = driver.findElement(By.id("kw"));
-		    WebElement search_button = driver.findElement(By.id("su"));
-
-		    search_text.sendKeys("Java");
-		    search_text.clear();
-		    search_text.sendKeys("Selenium");
-		    search_button.click();
-
-		    driver.quit();
-		  }
-		}
 		    WebDriver driver = new ChromeDriver();
 		    driver.get("https://www.baidu.com/");
 
