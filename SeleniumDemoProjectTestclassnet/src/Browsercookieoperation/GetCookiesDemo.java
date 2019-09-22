4e4eb6b243f8faa68ab857d6fc6466a85c22d2db
Kadeer Ainiwaer <49220464+anwarkadir76@users.noreply.github.com>
2019-09-22 15:12:05 -0400

    Update GetCookiesDemo.java

--- SeleniumDemoProjectTestclassnet/src/Browsercookieoperation/GetCookiesDemo.java
+++ SeleniumDemoProjectTestclassnet/src/Browsercookieoperation/GetCookiesDemo.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class GetCookiesDemo {
 
 	public static void main(String[] args){
-
+System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
 	    WebDriver driver = new ChromeDriver();
 	    driver.get("https://www.baidu.com");
 
