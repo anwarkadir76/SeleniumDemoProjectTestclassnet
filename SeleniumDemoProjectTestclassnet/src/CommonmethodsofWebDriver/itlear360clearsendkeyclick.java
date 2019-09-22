0bfed7ab280cb1be5fffd297b786db2d69d9f29f
Kadeer Ainiwaer <49220464+anwarkadir76@users.noreply.github.com>
2019-09-22 15:20:16 -0400

    Update itlear360clearsendkeyclick.java

--- SeleniumDemoProjectTestclassnet/src/CommonmethodsofWebDriver/itlear360clearsendkeyclick.java
+++ SeleniumDemoProjectTestclassnet/src/CommonmethodsofWebDriver/itlear360clearsendkeyclick.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class itlear360clearsendkeyclick {
 
 	 public static void main(String[] args) {
-
+System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
 		    WebDriver driver = new ChromeDriver();
 		    driver.get("https://www.itlearn360.com/");
 
