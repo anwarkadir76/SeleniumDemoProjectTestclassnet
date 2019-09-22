8e8200f7be4f81b9e97ad409405e2501b6513a35
Kadeer Ainiwaer <49220464+anwarkadir76@users.noreply.github.com>
2019-09-22 15:14:43 -0400

    Update GetSizeGetTextGetAttribute.java

--- SeleniumDemoProjectTestclassnet/src/CommonmethodsofWebDriver/GetSizeGetTextGetAttribute.java
+++ SeleniumDemoProjectTestclassnet/src/CommonmethodsofWebDriver/GetSizeGetTextGetAttribute.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class GetSizeGetTextGetAttribute {
 
 	 public static void main(String[] args) {
-
+		 System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
 		    WebDriver driver = new ChromeDriver();
 		    driver.get("https://www.baidu.com/");
 
