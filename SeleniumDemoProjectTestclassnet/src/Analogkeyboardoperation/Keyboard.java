4024bb7017492943df986d103d08a24619b76dde
Kadeer Ainiwaer <49220464+anwarkadir76@users.noreply.github.com>
2019-09-22 15:11:12 -0400

    Update Keyboard.java

--- SeleniumDemoProjectTestclassnet/src/Analogkeyboardoperation/Keyboard.java
+++ SeleniumDemoProjectTestclassnet/src/Analogkeyboardoperation/Keyboard.java
@@ -9,18 +9,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Keyboard {
 
 	public static void main(String[] args)throws InterruptedException {
-package CommonmethodsofWebDriver;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-public class ClearSendkeyClickWebDriver {
-
-	 public static void main(String[] args) {
-		 System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
-	
+		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
+		
 	    WebDriver driver = new ChromeDriver();
 	    driver.get("https://www.baidu.com");
 
